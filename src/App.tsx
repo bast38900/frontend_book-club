6c46ebc5bb904d45eb5d35c5368fcec910f8167d
Bast38900 <bast38900@edu.ucl.dk>
2023-09-21 10:48:16 +0200

    authetication_js_simple

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+
 import { Routes, Route, Link } from 'react-router-dom'
 import { HomePage } from './pages/HomePage';
 import { NotFoundPage } from './pages/NotFoundPage';
 import { Container, Navbar } from 'react-bootstrap';
 
+
 function App() {
+
   return (
     <>
       <Container>
@@ -12,7 +15,7 @@ function App() {
             <Link className="nav-link" to="/">Home</Link>
           </Container>
         </Navbar><br />
-      
+          
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="*" element={<NotFoundPage />} />
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,13 @@
+import User from '../components/User';
+import { useState } from 'react';
+
 export const HomePage = () => {
+  const [currUser, setCurrUser]=useState(null);
   return (
     <>
-      <h1>Hello World!</h1>
+      <h1>Welcome to Book Club</h1>
+      <br></br>
+      <User currUser={currUser} setCurrUser={setCurrUser} />
     </>
   )
 }
